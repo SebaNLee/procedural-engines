09c599959de2b6d73ef550b8269c57ee519007f9
SebaNLee <seba.nico.lee@gmail.com>
2026-01-05 20:16:16 -0300

    feat: vec2 std ops

--- boids-engine/src/math.rs
+++ boids-engine/src/math.rs
@@ -1,5 +1,6 @@
-use::std::ops{Add, Sub, Mult, AddAssign};
+use::std::ops::{Add, Sub, Mul, AddAssign};
 
+#[derive(Clone, Copy, Debug, Default)]
 pub struct Vec2 {
     pub x: f32,
     pub y: f32,
@@ -12,17 +13,32 @@ impl Vec2 {
 }
 
 impl Add for Vec2 {
+    type Output = Self;
 
+    fn add(self, rhs: Self) -> Self {
+        Self { x: self.x + rhs.x, y: self.y + rhs.y}
+    }
 }
 
 impl Sub for Vec2 {
-    
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self { x: self.x - rhs.x, y: self.y - rhs.y}
+    }
 }
 
-impl Mult for Vec2 {
-    
+impl Mul<f32> for Vec2 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self {
+        Self { x: self.x * rhs, y: self.y * rhs}
+    }
 }
 
 impl AddAssign for Vec2 {
-    
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
 }

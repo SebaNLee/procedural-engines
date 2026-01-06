4bc3f4210652728cdda76af29c1b9bff6c9328f9
SebaNLee <seba.nico.lee@gmail.com>
2026-01-05 21:11:56 -0300

    feat: Vec2 methods

--- boids-engine/src/math.rs
+++ boids-engine/src/math.rs
@@ -7,8 +7,25 @@ pub struct Vec2 {
 }
 
 impl Vec2 {
+
+    pub const ZERO: Self = Self { x: 0.0, y: 0.0 };
+
     pub fn new(x: f32, y: f32) -> Self {
         Self { x, y }
+    }
+
+    pub fn magnitude(self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    pub fn normalize(self) -> Self {
+        let magnitude = self.magnitude();
+
+        if magnitude > 0.0 {
+            Self { x: self.x / magnitude, y: self.y / magnitude }
+        } else {
+            Self::ZERO
+        }
     }
 }
 

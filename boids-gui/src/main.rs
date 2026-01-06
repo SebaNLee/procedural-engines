c9e2a34dbcd7f737b87c198403a24ec99ac73450
SebaNLee <seba.nico.lee@gmail.com>
2026-01-06 14:28:21 -0300

    feat!: minifb crate for gui debug

--- boids-gui/src/main.rs
+++ boids-gui/src/main.rs
@@ -1,4 +1,5 @@
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, MouseMode, Window, WindowOptions};
+use boids_engine::{World, Vec2};
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
@@ -7,7 +8,7 @@ fn main() {
     let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
     let mut window = Window::new(
-        "Test - ESC to exit",
+        "Boids - ESC to exit",
         WIDTH,
         HEIGHT,
         WindowOptions::default(),
@@ -16,17 +17,60 @@ fn main() {
         panic!("{}", e);
     });
 
-    // Limit to max ~60 fps update rate
     window.set_target_fps(60);
 
+    let mut world = World::new(200, WIDTH as f32, HEIGHT as f32);
+    let dt = 1.0 / 60.0;
+
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 1; // write something more funny here!
+
+        // mouse pos as attractor
+        if let Some((mx, my)) = window.get_mouse_pos(MouseMode::Discard) {
+            world.set_attractor(Some(Vec2::new(mx, my)));
+        } else {
+            world.clear_attractor();
         }
 
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        world.step(dt);
+
+        clear(&mut buffer, 0x000000);
+
+        for boid in world.get_boids() {
+            draw_pixel(
+                &mut buffer,
+                boid.pos.x as i32,
+                boid.pos.y as i32,
+                WIDTH as i32,
+                HEIGHT as i32,
+                0xFFFFFF,
+            );
+        }
+
         window
             .update_with_buffer(&buffer, WIDTH, HEIGHT)
             .unwrap();
     }
+}
+
+
+fn clear(buffer: &mut [u32], color: u32) {
+    for pixel in buffer.iter_mut() {
+        *pixel = color;
+    }
+}
+
+fn draw_pixel(
+    buffer: &mut [u32],
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    color: u32,
+) {
+    if x < 0 || y < 0 || x >= width || y >= height {
+        return;
+    }
+
+    let idx = (y * width + x) as usize;
+    buffer[idx] = color;
 }

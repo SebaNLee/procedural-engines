9d0e02d7af83a3f8e6864fc73bd576b1037c0495
SebaNLee <seba.nico.lee@gmail.com>
2026-01-06 12:53:25 -0300

    feat: adjust params

--- boids-engine/src/params.rs
+++ boids-engine/src/params.rs
@@ -3,7 +3,7 @@ pub struct Params {
     pub separation: f32,
     pub alignment: f32,
     pub cohesion: f32,
-    pub attraction: f32, // external force
+    pub attraction: f32, // aditional 4th rule, external force
 
     pub max_speed: f32,
     pub perception_radius: f32,
@@ -12,13 +12,13 @@ pub struct Params {
 impl Default for Params {
     fn default() -> Self {
         Self {
-            separation: 1.0,
-            alignment: 1.0,
-            cohesion: 1.0,
-            attraction: 0.0,
+            separation: 2.5,
+            alignment: 0.5,
+            cohesion: 3.0,
+            attraction: 15.0,
 
-            max_speed: 120.0,
-            perception_radius: 50.0, 
+            max_speed: 250.0,
+            perception_radius: 80.0,
         }
     }
 }

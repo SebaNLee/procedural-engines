78282e44a792deadf83a66d511eb4ed47a32be39
SebaNLee <seba.nico.lee@gmail.com>
2026-01-06 21:56:37 -0300

    refactor: rand lib fixed

--- boids-engine/src/random.rs
+++ boids-engine/src/random.rs
@@ -6,8 +6,7 @@ pub fn random_f32() -> f32 {
 
 #[cfg(not(target_arch = "wasm32"))]
 pub fn random_f32() -> f32 {
-    use std::time::{SystemTime, UNIX_EPOCH};
-    let nanos = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().subsec_nanos();
-    nanos as f32 % 1.0
+    use rand::Rng;
+    rand::thread_rng().r#gen()
 }
 

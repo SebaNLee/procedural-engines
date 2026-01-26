10098554beda9b183e927d381c02cd3180d04726
SebaNLee <seba.nico.lee@gmail.com>
2026-01-25 21:02:49 -0300

    chore: topography initial debug

--- gui/src/bin/topography.rs
+++ gui/src/bin/topography.rs
@@ -1,3 +1,19 @@
+use topography_engine::Topography;
+
 fn main() {
-    println!("Hello, world!");
+
+    let size = 9;
+
+    let mut topography = Topography::new(size, 10, 0.5, 0.5);
+
+    topography.compute();
+
+    let map = topography.get_map();
+
+    for y in 0..size {
+        for x in 0..size {
+            print!("{:5.2} ", map[x + y * size]);
+        }
+        println!();
+    }
 }
